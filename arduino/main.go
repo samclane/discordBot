64e4da6af678685183841fb01269a688ccd8160e
Sawyer McLane <samclane@gmail.com>
2020-03-16 08:39:12 -0600

    Added byte arrays for all displays required.

--- arduino/main.go
+++ arduino/main.go
@@ -2,17 +2,75 @@ package main
 
 import (
 	"github.com/samclane/drivers/max7219"
+	"time"
 )
+
+var X = [8]byte{
+	0b10000001,
+	0b01000010,
+	0b00100100,
+	0b00011000,
+	0b00011000,
+	0b00100100,
+	0b01000010,
+	0b10000001,
+}
+
+var PLAY = [8]byte{
+	0b00000000,
+	0b01100000,
+	0b01111000,
+	0b01111110,
+	0b01111111,
+	0b01111110,
+	0b01111000,
+	0b01100000,
+}
+
+var PAUSE = [8]byte{
+	0b00000000,
+	0b01100110,
+	0b01100110,
+	0b01100110,
+	0b01100110,
+	0b01100110,
+	0b01100110,
+	0b00000000,
+}
+
+var STOP = [8]byte{
+	0b00000000,
+	0b00000000,
+	0b00111100,
+	0b00111100,
+	0b00111100,
+	0b00111100,
+	0b00000000,
+	0b00000000,
+}
 
 func main() {
 	ma := max7219.New(2, 3, 4)
-	ma.MaxSingle(0x01, 0b10000001)
-	ma.MaxSingle(0x02, 0b01000010)
-	ma.MaxSingle(0x03, 0b00100100)
-	ma.MaxSingle(0x04, 0b00011000)
-	ma.MaxSingle(0x05, 0b00011000)
-	ma.MaxSingle(0x06, 0b00100100)
-	ma.MaxSingle(0x07, 0b01000010)
-	ma.MaxSingle(0x08, 0b10000001)
 
+	for i, row := range X {
+		ma.MaxSingle(byte(i+1), row)
+	}
+
+	time.Sleep(time.Second * 1)
+
+	for i, row := range PLAY {
+		ma.MaxSingle(byte(i+1), row)
+	}
+
+	time.Sleep(time.Second * 1)
+
+	for i, row := range PAUSE {
+		ma.MaxSingle(byte(i+1), row)
+	}
+
+	time.Sleep(time.Second * 1)
+
+	for i, row := range STOP {
+		ma.MaxSingle(byte(i+1), row)
+	}
 }
